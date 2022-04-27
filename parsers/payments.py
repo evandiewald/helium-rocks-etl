4748d1a459204c104de8b8162c06a20849c250c4
evandiewald <37876940+evandiewald@users.noreply.github.com>
2022-04-27 15:23:34 -0400

    compatibility

--- parsers/payments.py
+++ parsers/payments.py
@@ -9,38 +9,37 @@ class PaymentParser:
         self.transaction = transaction
 
     def insert(self, session: Session):
-        match self.transaction["type"]:
-            case "payment_v1":
-                parsed_payment = PaymentsParsed(
+        if self.transaction["type"] == "payment_v1":
+            parsed_payment = PaymentsParsed(
+                block=self.transaction["block"],
+                hash=self.transaction["hash"],
+                fee=self.transaction["fee"],
+                type=transaction_type.payment_v1,
+                payer=self.transaction["payer"],
+                payee=self.transaction["payee"],
+                amount=self.transaction["amount"]
+            )
+            try:
+                session.add(parsed_payment)
+                session.commit()
+            except IntegrityError:
+                session.rollback()
+                pass
+        elif self.transaction["type"] == "payment_v2":
+            parsed_payments = []
+            for payment in self.transaction["payments"]:
+                parsed_payments.append(PaymentsParsed(
                     block=self.transaction["block"],
                     hash=self.transaction["hash"],
                     fee=self.transaction["fee"],
-                    type=transaction_type.payment_v1,
+                    type=transaction_type.payment_v2,
                     payer=self.transaction["payer"],
-                    payee=self.transaction["payee"],
-                    amount=self.transaction["amount"]
-                )
-                try:
-                    session.add(parsed_payment)
-                    session.commit()
-                except IntegrityError:
-                    session.rollback()
-                    pass
-            case "payment_v2":
-                parsed_payments = []
-                for payment in self.transaction["payments"]:
-                    parsed_payments.append(PaymentsParsed(
-                        block=self.transaction["block"],
-                        hash=self.transaction["hash"],
-                        fee=self.transaction["fee"],
-                        type=transaction_type.payment_v2,
-                        payer=self.transaction["payer"],
-                        payee=payment["payee"],
-                        amount=payment["amount"]
-                    ))
-                try:
-                    session.add_all(parsed_payments)
-                    session.commit()
-                except IntegrityError:
-                    session.rollback()
-                    pass
+                    payee=payment["payee"],
+                    amount=payment["amount"]
+                ))
+            try:
+                session.add_all(parsed_payments)
+                session.commit()
+            except IntegrityError:
+                session.rollback()
+                pass
